f81fad65cf078b113cbfe137daf488a2bf8a7992
DenisCDev <coldgreen04@gmail.com>
2023-12-20 21:47:07 -0300

    fix: build

--- src/MapScreen.js
+++ src/MapScreen.js
@@ -1,55 +1,19 @@
-import React, { useState } from 'react'
-import MapView, { Marker } from 'react-native-maps';
-import { StyleSheet, View, Text, Dimensions } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-
-// const ASPECT_RATIO = width / height;
-// const LATITUDE = 37.78825;
-// const LONGITUDE = -122.4324;
-// const LATITUDE_DELTA = 0.0922;
-// const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
-// const SPACE = 0.01;
-
-
-function MapScreen() {
-
-  const [region, setRegion] = useState({
-    latitude: -23.006611,
-    longitude: -43.320278,
-    latitudeDelta: 0.009,
-    longitudeDelta: 0.009
-  });
-
-
+const MapScreen = () => {
   return (
-    <View style={styles.container}>
-      <MapView
-        style={styles.map}
-        region={region}
-        onRegionChangeComplete={region => setRegion(region)}
-      >
-        <Marker coordinate={{ latitude: -23.006611, longitude: -43.320278 }} />
-      </MapView>
-
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#e7e7e7",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Map Screen</Text>
     </View>
-  )
-}
+  );
+};
 
-export default MapScreen
-
-
-const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
-  marker: {
-    marginLeft: 46,
-    marginTop: 33,
-    fontWeight: 'bold',
-  },
-});
+export default MapScreen;
